9fa1763f39351d93d5d81f419872dea8c7f4b186
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2025-12-26 14:18:03 +0700

    fix code 03

--- frontend/src/features/dictionary/workers/dictWorker.ts
+++ frontend/src/features/dictionary/workers/dictWorker.ts
@@ -99,7 +99,7 @@ async function loadDictionary(dictPath : string): Promise<void> {
         } else {
             // Fetch from network
             console.log('[DictWorker] No valid cache found, fetching from network...');
-            if (dictPath == "") dictPath = "https://pub-494af1f931884f64b78688230a3db6e5.r2.dev/jmdict-eng-3.6.1.json";
+            if (dictPath === "") dictPath = "https://pub-494af1f931884f64b78688230a3db6e5.r2.dev/jmdict-eng-3.6.1.json";
 
             console.log(dictPath);
             const response = await fetch(dictPath);
