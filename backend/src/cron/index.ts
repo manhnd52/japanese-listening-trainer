69670bb3da7217b9c7b03c99d65491e91c224b06
Nguyen Duc Nghia <nghia.nd225895@sis.hust.edu.vn>
2025-12-27 11:49:53 +0700

    add note guide to test the email noti feature

--- backend/src/cron/index.ts
+++ backend/src/cron/index.ts
@@ -3,6 +3,7 @@ import { processDailyReminders } from '../services/reminder.service.js';
 
 export const initCronJobs = () => {
   cron.schedule('45 11 * * *', async () => {
+// nếu muốn test thì đổi ngày giờ ở trên thành ngày giờ muốn test
     await processDailyReminders();
   }, {
     timezone: "Asia/Ho_Chi_Minh"
